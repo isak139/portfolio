ba2614ede8a714d337e349714ef565961b0e546a
isak <t-yuta08780126@outlook.jp>
2022-12-14 18:29:13 +0900

    Create works.js

---
+++ pages/works.js
@@ -0,0 +1,90 @@
+import WorkCard from "../components/WorkCard";
+
+function works() {
+  return (
+    <section className="text-gray-700 border-b border-gray-200">
+      <div className="mx-auto container items-center max-w-screen-lg">
+        <h1 className="text-center text-3xl text-gray-900 mt-5">Works</h1>
+        <div className="flex flex-wrap">
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+          <WorkCard
+            title="カップル破壊ゲーム"
+            tags="#Unity"
+            image="/workImages/CoupleDestroyGame.webp"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default works;
