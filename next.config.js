8dc74126ad8bce1358eb54a2d97036b392950ed9
isak <t-yuta08780126@outlook.jp>
2022-12-15 01:35:35 +0900

    microcmsの画像を読み込み

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
 
 module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
