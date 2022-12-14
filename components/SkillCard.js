7ea6cbae82b88234b4fc1ab48d6cbba9760ff56f
isak <t-yuta08780126@outlook.jp>
2022-12-14 18:28:05 +0900

    ローカルSVGの入力に対応

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -1,12 +1,15 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function SkillCard({ icon, skillName, text }) {
+function SkillCard({ icon, SvgIcon, skillName, text }) {
   return (
     <div className="sm:w-1/2 md:w-1/3 w-full p-4">
-      <div className="bg-gray-200 rounded-lg p-5 h-48 sm:h-48">
+      <div className="bg-gray-200 rounded-lg p-6 h-48 sm:h-48">
         <div className="flex items-center mb-2">
-          <FontAwesomeIcon className="text-5xl" icon={icon} />
+          {icon ? (
+            <FontAwesomeIcon className="text-5xl text-gray-900" icon={icon} />
+          ) : (
+            <SvgIcon className="fill-gray-900" width="48" height="48" />
+          )}
           <h2 className="text-gray-900 text-3xl font-medium ml-2">
             {skillName}
           </h2>
