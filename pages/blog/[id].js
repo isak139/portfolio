ec6b3604654c9abfbc75466bc72703fd355fb62e
isak <t-yuta08780126@outlook.jp>
2022-12-15 23:57:48 +0900

    記事ページのレイアウトを修正

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,7 +5,6 @@ import Image from "next/image";
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
-
   return {
     props: {
       blog: data,
@@ -23,7 +22,12 @@ export default function blogId({ blog }) {
   return (
     <main className="w-2/3 mx-auto text-gray-700">
       <div className="relative h-48 sm:h-80 m-3">
-        <Image src={blog.eyecatch.url} alt="" fill className="object-cover" />
+        <Image
+          src={blog.eyecatch.url}
+          alt=""
+          fill
+          className="object-cover rounded-lg"
+        />
       </div>
       <h1 className="text-3xl text-center my-5">{blog.title}</h1>
       <div className="mb-5">投稿：{blog.publishedAt}</div>
