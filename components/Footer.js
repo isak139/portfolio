8e246108062bb8fe18309ef40636fad5ae5c30a6
isak <t-yuta08780126@outlook.jp>
2022-12-15 16:17:20 +0900

    フッターを追加

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="flex items-center justify-center bg-gray-300 py-1 mt-auto">
+      <p className="text-sm">Copyright ©2022- isak.</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
       <main>{children}</main>
-    </>
+      <Footer />
+    </div>
   );
 }
