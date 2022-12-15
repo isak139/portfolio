6ca76f3a22d55d783ed1bfed1ececfec480fbe93
isak <t-yuta08780126@outlook.jp>
2022-12-15 17:00:29 +0900

    アイコンを追加

--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,7 @@
 import WorkCard from "../components/WorkCard";
 import { client } from "../libs/client";
+import { faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "works" });
@@ -13,7 +15,10 @@ function works({ works }) {
   return (
     <section className="text-gray-700 border-b border-gray-200">
       <div className="mx-auto container items-center max-w-screen-lg">
-        <h1 className="text-center text-3xl text-gray-900 mt-5">Works</h1>
+        <h1 className="text-center text-3xl text-gray-900 mt-5">
+          <FontAwesomeIcon className="mx-3" icon={faWandMagicSparkles} />
+          Works
+        </h1>
         <div className="flex flex-wrap">
           {works.map((work) => (
             <WorkCard key={work.id} work={work} />
