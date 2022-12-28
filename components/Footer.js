9f9132db4034ffd22543aacf02a09dcd1aca7cf9
isak <t-yuta08780126@outlook.jp>
2022-12-28 21:23:16 +0900

    フッターの色味を変更

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-300 py-1 mt-auto">
+    <footer className="bg-gray-100 py-1 mt-auto">
       <p className="text-center text-sm">Copyright ©2022- isak.</p>
     </footer>
   );
