72fdff5f1a971aa58ee4991294007254aec7a9e6
isak <t-yuta08780126@outlook.jp>
2022-12-28 22:34:03 +0900

    リンクが無かった時のエラーを修正

--- components/WorkModal.js
+++ components/WorkModal.js
@@ -23,35 +23,39 @@ function WorkModal({ isModalOpen, setIsModalOpen, work }) {
         <p className="mb-3 mx-7">{work.description}</p>
         <div className="mt-auto">
           <h3 className="text-2xl ml-6 mb-3 text-center sm:text-left">Links</h3>
-          <div className="ml-6 mt-auto flex justify-center sm:justify-start">
-            {work.links.site ? (
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
-                onClick={() => window.open(work.links.site)}
-              >
-                <Icon icon="fa6-solid:link" className="mr-1" />
-                Site
-              </button>
-            ) : null}
-            {work.links.github ? (
-              <button
-                className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
-                onClick={() => window.open(work.links.github)}
-              >
-                <Icon icon="fa6-brands:github" className="mr-1" />
-                Github
-              </button>
-            ) : null}
-            {work.links.youtube ? (
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
-                onClick={() => window.open(work.links.youtube)}
-              >
-                <Icon icon="fa6-brands:youtube" className="mr-1" />
-                Youtube
-              </button>
-            ) : null}
-          </div>
+          {work.links ? (
+            <div className="ml-6 mt-auto flex justify-center sm:justify-start">
+              {"site" in work.links ? (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
+                  onClick={() => window.open(work.links.site)}
+                >
+                  <Icon icon="fa6-solid:link" className="mr-1" />
+                  Site
+                </button>
+              ) : null}
+              {"github" in work.links ? (
+                <button
+                  className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
+                  onClick={() => window.open(work.links.github)}
+                >
+                  <Icon icon="fa6-brands:github" className="mr-1" />
+                  Github
+                </button>
+              ) : null}
+              {"youtube" in work.links ? (
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-3 flex items-center"
+                  onClick={() => window.open(work.links.youtube)}
+                >
+                  <Icon icon="fa6-brands:youtube" className="mr-1" />
+                  Youtube
+                </button>
+              ) : null}
+            </div>
+          ) : (
+            <div className="ml-6">リンクはありません．</div>
+          )}
         </div>
       </div>
     </div>
