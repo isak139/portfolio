e5509ef4d1c73c919d8707a3c47d865658a2fe78
isak <t-yuta08780126@outlook.jp>
2022-12-28 20:33:53 +0900

    作品集のSEOを指定

--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
 import WorkCard from "../components/WorkCard";
 import { client } from "../libs/client";
 import { Icon } from "@iconify/react";
+import { NextSeo } from "next-seo";
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "works" });
@@ -12,19 +13,30 @@ export const getStaticProps = async () => {
 
 function works({ works }) {
   return (
-    <section className="text-gray-700 border-b border-gray-200">
-      <div className="mx-auto container items-center max-w-screen-lg">
-        <h1 className="text-3xl text-gray-900 mt-5 flex flex-row items-center justify-center">
-          <Icon icon="fa6-solid:wand-magic-sparkles" className="mr-3" />
-          Works
-        </h1>
-        <div className="flex flex-wrap">
-          {works.map((work) => (
-            <WorkCard key={work.id} work={work} />
-          ))}
+    <>
+      <NextSeo
+        title="Works | isak's portfolio"
+        description="isakの作品集です"
+        openGraph={{
+          url: "https://isak-portfolio.vercel.app/works",
+          title: "Works | isak's portfolio",
+          description: "isakの作品集です",
+        }}
+      />
+      <section className="text-gray-700 border-b border-gray-200">
+        <div className="mx-auto container items-center max-w-screen-lg">
+          <h1 className="text-3xl text-gray-900 mt-5 flex flex-row items-center justify-center">
+            <Icon icon="fa6-solid:wand-magic-sparkles" className="mr-3" />
+            Works
+          </h1>
+          <div className="flex flex-wrap">
+            {works.map((work) => (
+              <WorkCard key={work.id} work={work} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
