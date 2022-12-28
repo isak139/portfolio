7fc11d816d752acaeffec6a27f39e73c438729b0
isak <t-yuta08780126@outlook.jp>
2022-12-28 21:23:27 +0900

    スキルカードの色味を変更

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 function SkillCard({ icon, skillName, text }) {
   return (
     <div className="sm:w-1/2 md:w-1/3 w-full p-4">
-      <div className="bg-gray-200 rounded-lg p-6 h-48 sm:h-48">
+      <div className="bg-gray-100 rounded-lg p-6 h-48 sm:h-48">
         <div className="flex items-center mb-2 text-gray-900">
           <Icon icon={icon} className="text-5xl " />
           <h2 className="text-3xl font-medium ml-2">{skillName}</h2>
