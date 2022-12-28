7ed50ead52482a2d3bbaa77a552504ab3eefd945
isak <t-yuta08780126@outlook.jp>
2022-12-28 20:32:54 +0900

    デフォルトSEOを指定

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <DefaultSeo
+        defaultTitle="isak's portfolio"
+        description="isakの作品集やブログを掲載したポートフォリオサイトです"
+        openGraph={{
+          type: "website",
+          title: "isak's portfolio",
+          description: "isakの作品集やブログを掲載したポートフォリオサイトです",
+          site_name: "isak's portfolio",
+          url: "https://isak-portfolio.vercel.app/",
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
