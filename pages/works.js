faa8c4048d69595f4ea4c4e04cf479b9959caa9a
isak <t-yuta08780126@outlook.jp>
2022-12-29 20:03:53 +0900

    作品のタグ検索機能を追加

--- pages/works.js
+++ pages/works.js
@@ -2,8 +2,10 @@ import WorkCard from "../components/WorkCard";
 import { client } from "../libs/client";
 import { Icon } from "@iconify/react";
 import { NextSeo } from "next-seo";
-import WorkTagAccordion from "../components/WorkTagAccordion";
+import WorkTagMenu from "../components/WorkTagMenu";
 import { useState } from "react";
+import TagMenuButton from "../components/TagMenuButton";
+import tags from "../libs/tags.json";
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "works" });
@@ -14,7 +16,12 @@ export const getStaticProps = async () => {
 };
 
 function Works({ works }) {
-  const [selectedTag, setSelectedTag] = useState("");
+  const [selectedTag, setSelectedTag] = useState("All");
+  const [isOpenTagMenu, setIsOpenTagMenu] = useState(false);
+  const displayWorks =
+    selectedTag === "All"
+      ? works
+      : works.filter((work) => work.tags.includes(selectedTag));
   return (
     <>
       <NextSeo
@@ -32,9 +39,31 @@ function Works({ works }) {
             <Icon icon="fa6-solid:wand-magic-sparkles" className="mr-3" />
             Works
           </h1>
-          <WorkTagAccordion />
-          <div className="flex flex-wrap">
-            {works.map((work) => (
+          {/* tagsドロップダウンメニュー */}
+          <div className="flex flex-col relative z-10">
+            <button
+              onClick={() => setIsOpenTagMenu(!isOpenTagMenu)}
+              className="bg-gray-100 outline outline-1 outline-gray-200 px-4 py-1 ml-auto mr-6 rounded-md hover:bg-gray-200 duration-300 flex items-center"
+            >
+              tags <Icon icon="fa6-solid:caret-down" className="ml-2" />
+            </button>
+            {isOpenTagMenu && (
+              <div className="p-3 w-fit right-6 top-12 bg-white rounded-lg outline outline-1 outline-gray-200 absolute">
+                <ul className="w-32 text-center">
+                  {tags.map((tag) => (
+                    <TagMenuButton
+                      key={tag}
+                      tag={tag}
+                      selectedTag={selectedTag}
+                      setSelectedTag={setSelectedTag}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-wrap z-0">
+            {displayWorks.map((work) => (
               <WorkCard key={work.id} work={work} />
             ))}
           </div>
