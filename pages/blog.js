d3f4a18c236ce32620190e569d8289ff3244c472
isak <t-yuta08780126@outlook.jp>
2022-12-29 20:10:59 +0900

    ブログページにも検索機能を追加

--- pages/blog.js
+++ pages/blog.js
@@ -4,6 +4,9 @@ import { client } from "../libs/client";
 import { Icon } from "@iconify/react";
 import BlogCard from "../components/BlogCard";
 import { NextSeo } from "next-seo";
+import { useState } from "react";
+import TagMenuButton from "../components/TagMenuButton";
+import tags from "../libs/tags.json";
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
@@ -14,6 +17,12 @@ export const getStaticProps = async () => {
 };
 
 function Blog({ blog }) {
+  const [selectedTag, setSelectedTag] = useState("All");
+  const [isOpenTagMenu, setIsOpenTagMenu] = useState(false);
+  const displayBlog =
+    selectedTag === "All"
+      ? blog
+      : blog.filter((blog) => blog.tags.includes(selectedTag));
   return (
     <>
       <NextSeo
@@ -31,36 +40,34 @@ function Blog({ blog }) {
             <Icon icon="fa6-solid:rss" className="mr-3" />
             Blog
           </h1>
+          {/* tagsドロップダウンメニュー */}
+          <div className="flex flex-col relative z-10">
+            <button
+              onClick={() => setIsOpenTagMenu(!isOpenTagMenu)}
+              className="bg-gray-100 outline outline-1 outline-gray-200 px-4 py-1 ml-auto mr-6 rounded-md hover:bg-gray-200 duration-300 flex items-center"
+            >
+              tags <Icon icon="fa6-solid:caret-down" className="ml-2" />
+            </button>
+            {isOpenTagMenu && (
+              <div className="p-3 w-fit right-6 top-12 bg-white rounded-lg outline outline-1 outline-gray-200 absolute">
+                <ul className="w-32 text-center">
+                  {tags.map((tag) => (
+                    <TagMenuButton
+                      key={tag}
+                      tag={tag}
+                      selectedTag={selectedTag}
+                      setSelectedTag={setSelectedTag}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
           <div className="flex flex-wrap">
-            {blog.map((blog) => (
+            {displayBlog.map((blog) => (
               <BlogCard key={blog.id} blog={blog} />
             ))}
           </div>
-          {/* <div className="flex flex-col mx-10 mt-5">
-          {blog.map((blog) => (
-            <Link
-              key={blog.id}
-              href={`blog/${blog.id}`}
-              className="flex flex-col  items-center sm:items-start sm:flex-row m-3 p-4 hover:bg-gray-100 hover:rounded-lg transition duration-5"
-            >
-              <div className="w-11/12 h-48 sm:w-48 sm:h-32 relative ">
-                <Image
-                  src={blog.eyecatch.url}
-                  alt=""
-                  fill
-                  className="object-cover rounded-lg"
-                />
-              </div>
-              <div className="md:m-3 text-xl p-2 flex flex-col">
-                <h2>{blog.title}</h2>
-                <div className="sm:mt-10 sm:ml-5 flex flex-row items-center justify-center sm:justify-start">
-                  <Icon icon="fa6-solid:arrow-right" className="mr-3" />
-                  <p className="text-lg">記事を読む</p>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div> */}
         </div>
       </section>
     </>
