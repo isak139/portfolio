cedb9d65d084d95dea96ab83523a67bcf17fdcba
isak <t-yuta08780126@outlook.jp>
2023-01-06 22:46:47 +0900

    リンクの無いaタグを修正

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ function Header() {
   return (
     <header className="sticky top-0 z-50 text-gray-700 border-b border-gray-200 bg-white">
       <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
-        <a className="font-medium text-gray-900 mb-4 md:mb-0">
+        <div className="font-medium text-gray-900 mb-4 md:mb-0">
           <span className="text-xl ml-3">isak&apos;s portfolio</span>
-        </a>
+        </div>
         <nav className="md:ml-auto text-base">
           <Link
             href="/"
